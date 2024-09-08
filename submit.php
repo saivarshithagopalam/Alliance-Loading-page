fe672b4facb30c7c38a41d680714fd5b449ed8d4
saivarshithagopalam <saivarshithagopalam@gmail.com>
2024-09-08 15:11:06 +0530

    Done

---
+++ submit.php
@@ -0,0 +1,28 @@
+<?php
+// submit.php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "university";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO applications (name, email, message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
